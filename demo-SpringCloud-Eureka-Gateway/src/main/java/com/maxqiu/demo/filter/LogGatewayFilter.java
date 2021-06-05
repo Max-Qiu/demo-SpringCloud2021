61ad9da2d7168c1b3873942d8165b459ac6e77fd
Max-Qiu <qzhing@126.com>
2021-06-05 23:05:42 +0800

    Gateway示例

---
+++ demo-SpringCloud-Eureka-Gateway/src/main/java/com/maxqiu/demo/filter/LogGatewayFilter.java
@@ -0,0 +1,25 @@
+package com.maxqiu.demo.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Max_Qiu
+ */
+@Component
+public class LogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
