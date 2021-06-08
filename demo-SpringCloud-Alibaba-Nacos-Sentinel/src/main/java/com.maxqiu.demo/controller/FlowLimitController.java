86fac5e566985901b9f41d60e99492ce7a3bea77
Max-Qiu <qzhing@126.com>
2021-06-08 21:19:23 +0800

    Sentinel示例

---
+++ demo-SpringCloud-Alibaba-Nacos-Sentinel/src/main/java/com.maxqiu.demo/controller/FlowLimitController.java
@@ -0,0 +1,24 @@
+package com.maxqiu.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Max_Qiu
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+}
